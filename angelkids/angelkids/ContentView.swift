8bca23940943508df6086acd071bb557f3b8525d
reymesson1 <rey_messon@hotmail.com>
2024-08-28 23:04:36 -0400

    initial data

---
+++ angelkids/angelkids/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  angelkids
+//
+//  Created by user250318 on 8/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ angelkids/angelkids/angelkidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  angelkidsApp.swift
+//  angelkids
+//
+//  Created by user250318 on 8/28/24.
+//
+
+import SwiftUI
+
+@main
+struct angelkidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
